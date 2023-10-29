8c71ba0cd99b0875dca15f3599358a30d76ddfbd
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro-2.local>
2023-10-29 11:41:12 -0700

    py-test continued

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -46,34 +46,18 @@ export async function GET(request: NextRequest) {
   const json = {
     greeting,
   };
-  console.log(request.url);
-  const query = request.url.split("?")[1];
-  const searchParams = new URLSearchParams(query);
 
-  const address = searchParams.get("address");
-  const priceBedBath = searchParams.get("priceBedBath");
-  const builtUpLotSize = searchParams.get("builtUpLotSize");
-  const fileName = searchParams.get("fileName");
-
-  if (address == null || priceBedBath == null || builtUpLotSize == null) {
-    console.log("returning from execute API call");
-    return NextResponse.json(json);
-  }
   json.greeting = await new Promise((resolve, reject) => {
     exec(`python myscript-2.py`, (error, stdout, stderr) => {
       if (error) {
-        console.error(`exec error: ${error}`);
-        json.greeting = "err";
+        json.greeting = "myscript-2 failed";
         reject(error);
       } else {
-        json.greeting = "eee";
-        console.log("stdout");
-        console.log(stdout);
+        json.greeting = "myscript-2 executed successfully";
         resolve(stdout);
       }
     });
   });
 
-  console.log("inside get call");
   return NextResponse.json(json);
 }
