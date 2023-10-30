4d25709e26fabd3df98d2982342b195d26527d14
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro-2.local>
2023-10-29 19:53:37 -0700

    testing PIL

--- test.py
+++ test.py
@@ -4,4 +4,9 @@ import requests
 import sys
 import base64
 
+ 
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+img = Image.new("RGB", (11, 11))
+
 print("hello from python, her are the args " + arg1 + " " + arg2)
