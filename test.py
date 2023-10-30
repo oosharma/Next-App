356def767bb78fa99e502d648eb833f59898a566
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro-2.local>
2023-10-29 19:22:52 -0700

    testing-imports

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+import requests
 import sys
+import base64
+
+base64_encoded = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
 
 arg1 = sys.argv[1]
 arg2 = sys.argv[2]
